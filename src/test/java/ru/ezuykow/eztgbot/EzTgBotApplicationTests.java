725bfd63bf97559929a16a6fa7232ec015084363
ezuykow <zuy1996@gmail.com>
2024-07-17 15:57:26 +0300

    Initial commit

---
+++ src/test/java/ru/ezuykow/eztgbot/EzTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ezuykow.eztgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzTgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
