ec9771992690832b57ae3a59d15bf8e647ae6630
ezuykow <zuy1996@gmail.com>
2025-06-30 13:21:54 +0300

    - Добавил флаг skipAllUpdates в ProcessorSwitcher - Поднял версию до 0.2.2

--- src/main/java/ru/ezuykow/eztgbot/processing/ProcessorSwitcher.java
+++ src/main/java/ru/ezuykow/eztgbot/processing/ProcessorSwitcher.java
@@ -1,6 +1,7 @@
 package ru.ezuykow.eztgbot.processing;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 import ru.ezuykow.eztgbot.context.EzTgBotContext;
 import ru.ezuykow.eztgbot.processing.processors.EzTgBotProcessor;
@@ -20,18 +21,28 @@ public class ProcessorSwitcher {
     private final List<EzTgBotProcessor> processors;
 
     /**
+     * Флаг, который можно установить в true для пропуска обработки всех апдейтов.
+     * По-умолчанию false
+     */
+    @Setter
+    private boolean skipAllUpdates = false;
+
+    /**
      * Вызывает подходящий обработчик для текущего апдейта
      */
     public void callSuitableProcessor() {
-        Optional<EzTgBotProcessor> suitableProcessor =
-                processors.stream()
-                        .filter(p -> p.suitFor() == EzTgBotContext.getUpdateContentType())
-                        .findAny();
+        if (!skipAllUpdates) {
+            Optional<EzTgBotProcessor> suitableProcessor =
+                    processors.stream()
+                            .filter(p -> p.suitFor() == EzTgBotContext.getUpdateContentType())
+                            .findAny();
 
-        suitableProcessor.ifPresentOrElse(
-                EzTgBotProcessor::process,
-                () -> EzTgBotLogger.info("Suitable processor for [{}] update not found!",
-                        EzTgBotContext.getUpdateContentType().toString())
-        );
+            suitableProcessor.ifPresentOrElse(
+                    EzTgBotProcessor::process,
+                    () -> EzTgBotLogger.info("Suitable processor for [{}] update not found!",
+                            EzTgBotContext.getUpdateContentType().toString())
+            );
+        }
     }
+
 }
